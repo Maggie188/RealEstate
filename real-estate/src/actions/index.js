6e78679ad97868af7e4f2f26484ac358872e297e
Maggie <mengjing0818@gmail.com>
2021-09-03 14:34:20 -0700

    All action creator.

--- real-estate/src/actions/index.js
+++ real-estate/src/actions/index.js
@@ -1,9 +1,43 @@
 import homes from '../apis/homes';
-import { CREATE_HOME } from './type';
+import { 
+    CREATE_HOME,
+    FETCH_HOME,
+    FETCH_HOMES,
+    DELETE_HOME,
+    EDIT_HOME
+ } from './types';
 
 
 export const createHome = formValues => async dispatch => {
     const response = await homes.post('/homes', formValues);
 
     dispatch({ type: CREATE_HOME, payload: response.data });
+};
+
+
+export const fetchHomes = () => async dispatch => {
+    const response = await homes.get('/homes');
+
+    dispatch({ type: FETCH_HOMES, payload: response.data });
+};
+
+
+export const fetchHome = id => async dispatch => {
+    const response = await homes.get(`/homes/${id}`);
+
+    dispatch({ type: FETCH_HOME, payload: response.data });
+};
+
+
+export const editHome = (id, formValues) => async dispatch => {
+    const response = await homes.put(`/homes/${id}`, formValues);
+
+    dispatch({ type: EDIT_HOME, payload: response.data });
+};
+
+
+export const deleteHome = id => async dispatch => {
+    await homes.delete(`/homes/${id}`);
+
+    dispatch({ type: DELETE_HOME, payload: id });
 };
--- real-estate/src/actions/type.js
+++
@@ -1,1 +0,0 @@
-export const CTREATE_HOME = 'CREATE_HOME';
