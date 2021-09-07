816fd0eb4b6a5b5eddf477ba63ded2324a6b97d2
Maggie <mengjing0818@gmail.com>
2021-09-06 17:26:30 -0700

    Refactor home creation.

--- real-estate/src/components/homes/HomeCreate.js
+++ real-estate/src/components/homes/HomeCreate.js
@@ -1,90 +1,23 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createHome } from '../../actions';
+import HomeForm from './HomeForm';
 
 
 class HomeCreate extends React.Component {
-   renderError({ error, touched }) {
-      if (touched && error) {
-         return (
-            <div className="ui error message">
-               <div className="header">{error}</div>
-            </div>
-         );
-      }
-   }
-
-   renderInput = ({ input, label, meta }) => {
-      const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
-
-      return (
-         <div className={className}>
-            <label>{label}</label>
-            <input {...input} />
-            {this.renderError(meta)}
-         </div>
-      )
-   }
-
-   renderSelect({ label }) {
-      return  (
-         <div className="field">
-            <label>{label}</label>
-            <select className="ui fluid search dropdown">
-               <option value="">Select Home Type</option>
-               <option value="HO">Houses</option>
-               <option value="TH">Townhomes</option>
-               <option value="MF">Multi-family</option>
-               <option value="CO">Condons/Co-ops</option>
-               <option value="LO">Lots/Land</option>
-               <option value="AP">Apartments</option>
-               <option value="MA">Manufactured</option>
-            </select>
-         </div>
-      )
-   }
-
    onSubmit = formValues => {
       this.props.createHome(formValues);
    };
 
-
-
    render() {
       return (
-         <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-            <Field name="address" component={this.renderInput} label="Address" />
-            <Field name="price" component={this.renderInput} label="Price ($)" />
-            <Field name="year" component={this.renderInput} label="Year Built" />
-            <Field name="type" component={this.renderSelect} label="Home Type" />
-            <Field name="bed" component={this.renderInput} label="Number of Bedroom" />
-            <Field name="bath" component={this.renderInput} label="Number of Bathroom" />
-            <Field name="squarefeet" component={this.renderInput} label="Square Feet" />
-            <Field name="lot" component={this.renderInput} label="Lot Size" />
-            <button className="ui right floated button primary">Submit</button>
-         </form>
+         <div>
+            <h3>Add a Home</h3>
+            <HomeForm onSubmit={this.onSubmit} />
+         </div>
+         
       );
    }
 };
 
-const validate = formValues => {
-   const errors = {};
-
-   if (!formValues.address) {
-      errors.address = 'Please enter the address';
-   }
-
-   if (!formValues.price) {
-      errors.price = 'Please enter the price';
-   }
-
-   return errors;
-};
-
-const formWrapped = reduxForm({
-   form: 'homeCreate',
-   validate
-})(HomeCreate);
-
-export default connect(null, { createHome })(formWrapped);
+export default connect(null, { createHome })(HomeCreate);
