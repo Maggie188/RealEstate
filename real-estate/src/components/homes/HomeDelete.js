ea1eed107404a1dbfcecef41ed869d6e7ddb7307
Maggie <mengjing0818@gmail.com>
2021-09-06 22:51:15 -0700

    Make modal reusable and adjust the two buttons position issue.

--- real-estate/src/components/homes/HomeDelete.js
+++ real-estate/src/components/homes/HomeDelete.js
@@ -3,7 +3,22 @@ import Modal from '../Modal';
 
 
 const HomeDelete = () => {
-   return <div><Modal /></div> 
+   const actions = (
+      <React.Fragment>
+         <button className="ui button">Cancel</button>
+         <button className="ui primary button">Delete</button>
+      </React.Fragment>
+   );
+
+   return (
+      <div>
+         <Modal 
+            title="Delete Home"
+            content="Are you sure you want to delete this home?"
+            actions={actions}
+         />
+      </div>
+   ); 
 };
 
 export default HomeDelete;
