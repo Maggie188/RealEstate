e2d7109a65d2a011e101da049dcda2bcc4340dda
Maggie <mengjing0818@gmail.com>
2021-09-08 17:23:28 -0700

    Property tax calculation.

--- real-estate/src/components/homes/MonthlyCost.js
+++ real-estate/src/components/homes/MonthlyCost.js
@@ -8,7 +8,8 @@ class MonthlyCost extends React.Component {
       downpayamt: 0,
       downpayrate: 20,
       interest: 2.828,
-      loanterm: "1"
+      loanterm: "1",
+      taxrate: 0.81
    }
 
 
@@ -35,16 +36,20 @@ class MonthlyCost extends React.Component {
 
    downpayamtChange = e => {
       const value = e.target.value
-      if (this.state.downpayamt !== value)
+      if (!value)
+         this.setState({ downpayamt : 0 })
+      else if (this.state.downpayamt !== value)
          this.setState({ downpayamt : value })
-      const downPayRate = value / this.state.homeprice
+      const downPayRate = value / this.state.homeprice * 100
       if (this.state.downpayrate !== downPayRate)
          this.setState({ downpayrate : downPayRate })
    }
 
    downpayrateChange = e => {
       const value = e.target.value
-      if (this.state.downpayrate !== value)
+      if (!value)
+         this.setState({ downpayrate : 0 })
+      else if (this.state.downpayrate !== value)
          this.setState({ downpayrate : value})
       const downPayAmt = value * this.state.homeprice / 100
       if (this.state.downpayamt !== downPayAmt)
@@ -59,9 +64,20 @@ class MonthlyCost extends React.Component {
 
    interestChange = e => {
       const value = e.target.value
-      if (this.state.interest !== value)
+      if (!value)
+         this.setState({ interest : 0 })
+      else if (this.state.interest !== value)
          this.setState({ interest : value })
    }
+
+   taxratechange = e => {
+      const value = e.target.value
+      if (!value)
+         this.setState({ taxrate : 0 })
+      else if (this.state.taxrate !== value)
+         this.setState({ taxrate : value })
+   }
+   
 
    render() {
       if (!this.props.home) {
@@ -71,8 +87,8 @@ class MonthlyCost extends React.Component {
       console.log(this.state)
 
       const price = this.props.home.price;
-      // const downpayment = parseInt(this.state.homeprice, 10) * this.state.downpayrate / 100
-
+      
+      
       const monthlyInterest = this.state.interest / 1200
       const numberOfMonths = `${this.state.loanterm === "1" ? 360 : 180}`
       const listPrice = parseInt(this.state.homeprice, 10)
@@ -80,6 +96,10 @@ class MonthlyCost extends React.Component {
       const tmp = (1 + monthlyInterest) ** numberOfMonths
       const tmppayment = principal * ((monthlyInterest * tmp) / (tmp - 1))
       const payment = tmppayment.toFixed(2)
+
+      const tmpYearlyPropTax = this.state.homeprice * this.state.taxrate / 100
+      const yearlyPropTax = tmpYearlyPropTax.toFixed(2)
+      const monthlyPropTax = yearlyPropTax / 12
 
       return (
          <div>
@@ -97,7 +117,7 @@ class MonthlyCost extends React.Component {
                <div className="title active">
                   <i className="dropdown icon"></i>
                   <span>Principal & interest</span>
-                  <span className="ui right floated header">{`$ ${payment}`}</span>
+                  <span className="ui right floated header">{`$ ${payment} / mo`}</span>
                </div>
                <div className="active content">
                   <form className="ui form">
@@ -145,6 +165,70 @@ class MonthlyCost extends React.Component {
                      </div>
                   </form>
                </div>
+
+               <div className="title active">
+                  <i className="dropdown icon"></i>
+                  <span>Property taxes</span>
+                  <span className="ui right floated header">{`$ ${monthlyPropTax}`}</span>
+               </div>
+               <div className="active content">
+                  <div class="ui equal width padded grid">
+                     <div className="row">
+                        <div className="left aligned five wide column">
+                           Home price
+                        </div>
+                        <div className="left aligned ten wide column">
+                           Tax rate
+                        </div>
+                     </div>
+                     <div className="row">
+                        <div className="left middle aligned three wide column">
+                           {`$ ${this.state.homeprice}`}
+                        </div>
+                        <div className="left middle aligned two wide column">
+                           ✖️
+                        </div>
+                        <div className="ui right labeled mini input three wide colum">
+                           <input type="float" value={this.state.taxrate} onChange={(e) => this.taxratechange(e)}/>
+                           <div className="ui basic label">%</div>
+                        </div>
+                        <div className="center middle aligned one wide column">
+                           =
+                        </div>
+                        <div className="center middle aligned four wide column">
+                           {`$ ${yearlyPropTax} / year`}
+                        </div>
+                     </div>
+                  </div>
+               </div>
+
+               <div className="title active">
+                  <i className="dropdown icon"></i>
+                  <span>Home insurance</span>
+                  <span className="ui right floated header">$</span>
+               </div>
+               <div className="active content">
+
+               </div>
+
+               <div className="title active">
+                  <i className="dropdown icon"></i>
+                  <span>HOA fees</span>
+                  <span className="ui right floated header">$</span>
+               </div>
+               <div className="active content">
+
+               </div>
+
+               <div className="title active">
+                  <i className="dropdown icon"></i>
+                  <span>Utilities</span>
+                  <span className="ui right floated header">$</span>
+               </div>
+               <div className="active content">
+
+               </div>
+
             </div>
          </div>
       )
